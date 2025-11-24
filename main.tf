869c5e574afbd2b9962e6bf1aada127fd384002c
narayas <narayas@autodesk.com>
2025-11-23 18:37:31 -0800

    Added the base code for this project

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  profile = "default"
-  region  = var.region
-}
-
 # Random string generator
 resource "random_id" "randomID" {
   byte_length = 8
---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
